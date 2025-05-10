4d9c7b99c72c2d7a2319a9a906553a5ca26f204f
faisalEsMagico <faisal.khan@esmagico.in>
2025-05-11 00:44:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -11,23 +11,26 @@ import { Contact } from "./Components/Contact/Contact";
 import { Footer } from "./Components/Footer/Footer";
 import { ScrollToTop } from "./Components/ScrollToTop/ScrollToTop";
 import { Resume } from "./Components/Resume/Resume";
+
 export default function App() {
   const [{ themename }] = React.useContext(ThemeContext);
   React.useEffect(() => {
     Aos.init({ duration: 1500 });
   }, []);
+
   return (
     <div id="top" className={`${themename} app`}>
       <section id="#home">
         <Header />
       </section>
+
       <main>
         <About />
         <Github />
         <section id="#projects">
           <Projects />
         </section>
-        <Resume/>
+        <Resume />
         <section id="#contact">
           <Contact />
         </section>
